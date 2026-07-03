e7409f4bcea4d2ed1e3a1016ebe834ceb1aa3709
gouravsharma199 <gouravsharma7227199@gmail.com>
2026-07-03 23:18:11 +0530

    digit count

--- palindrome.js
+++ palindrome.js
@@ -2,15 +2,13 @@ function palindrome(num){
     let rev= 0;
     let rem = 0;
     let numCopy = num;
-    while(num>0){
-        
 
+    while(num>0){      
         rem = num%10;
         rev = (rev*10)+rem;
         num = Math.floor(num/10);
-
-
     }
+    
     if(rev==numCopy){
         console.log("palindrome");
     }else{
