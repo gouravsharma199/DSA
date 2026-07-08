710a53233f3a01db000d2cc2f7f7b22c1d576c8f
gouravsharma199 <gouravsharma7227199@gmail.com>
2026-07-08 23:33:01 +0530

    again

--- 01-First.js
+++ 01-First.js
@@ -94,9 +94,11 @@ function stringCounter(str){
 
 function loop(num){
 
+   
     for(let i=0;i<num;i++){
+
        for(let j =0;j<i;j++){
-         console.log(i +"and"+j);
+         console.log(i +" and "+j);
        } 
     }
 
