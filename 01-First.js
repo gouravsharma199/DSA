94aa3b2835a8f8f82d7acd89e7fa06089744af93
gouravsharma199 <gouravsharma7227199@gmail.com>
2026-07-08 15:28:33 +0530

    odd even

--- 01-First.js
+++ 01-First.js
@@ -4,26 +4,23 @@
 
 
 // //Add 2 number and print on console.log
-// function addSum(a,b){
-//     return a+b;
-// }
+function addSum(a,b){
+    return a+b;
+}
 
 // console.log(addSum(1,5));
 
 
 
-
-
-
 // // check even and odd number 
-// function oddEven(number){
-//     if(number%2 == 0){
-//         console.log("Even Number");
-//     }
-//     else{
-//         console.log("Odd number")
-//     }
-// }
+function oddEven(number){
+    if(number%2 == 0){
+        console.log("Even Number");
+    }
+    else{
+        console.log("Odd number")
+    }
+}
 
 // oddEven(5);
 
@@ -33,16 +30,16 @@
 
 // //check the smaller number from 2 numbers
 
-// function smallNumber(number1,number2){
-//     if(number1>number2){
-//         console.log(number1,"is big number");
-//     }
-//     else{
-//         console.log(number2,"is bigger number")
-//     }
-//     //ORRRR
-//     // return number1>number2?console.log(number1): console.log(number2);
-// }
+function smallNumber(number1,number2){
+    if(number1>number2){
+        console.log(number1,"is big number");
+    }
+    else{
+        console.log(number2,"is bigger number")
+    }
+    //ORRRR
+    // return number1>number2?console.log(number1): console.log(number2);
+}
 
 // smallNumber(7,5);
 
@@ -53,14 +50,13 @@
 
 
 //Print number by the Loop 
+ function printNumber(num){
+  for(i=1;i<=num;i++){
+    console.log(i);
+  }
+}
 
-        // function printNumber(num){
-        //     for(i=1;i<=num;i++){
-        //         console.log(i);
-        //     }
-        // }
-
-        // printNumber(20);
+// printNumber(20);
 
 
 
@@ -68,13 +64,13 @@
 
 // //Sum of the Array 
 
-// function sumArray(arr) {
-//   let sum = 0;
-//   for (let num of arr) {
-//     sum += num;
-//   }
-//   return console.log(sum);
-// }
+function sumArray(arr) {
+  let sum = 0;
+  for (let num of arr) {
+    sum += num;
+  }
+  return console.log(sum);
+}
 
 // sumArray([1, 2, 3, 4])
 
@@ -84,16 +80,15 @@
 
 // Count Caracter from String 
 
-//     function stringCounter(str){
-//         return console.log(str.length);
-
-//     }
+function stringCounter(str){
+  return console.log(str.length);
+}
 //     stringCounter("gouravJavascript");
 
 
+
+
 // find the index of element 
-
-
 
 function loop(num){
 
@@ -105,4 +100,4 @@ function loop(num){
 
 }
 
-loop(5);
+// loop(5);
