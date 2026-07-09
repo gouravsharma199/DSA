28b12d12cd3d97715ea8997a79558c5ae57bd7b8
gouravsharma199 <gouravsharma7227199@gmail.com>
2026-07-09 23:50:02 +0530

    pattern again started

--- 02-pattern.js
+++
@@ -1,5 +0,0 @@
-//Print dimand shape in console 
-
-for(let i=0;i<7;i++){
-    
-}
---
+++ 2-pattern.js
@@ -0,0 +1,24 @@
+
+//    *
+//   **
+//  ***
+// ****
+//*****
+
+
+function newStar(n){
+    for(let i=0;i<n;i++){
+    
+        let row = "";
+        for(let j=0;j<n-(i+1);j++){
+            row = row+" ";
+            
+        }
+        for(let k=0;k<i+1;k++){
+                row = row+"*";
+            }
+        console.log(row) ;
+    
+    }
+}
+newStar(5);
