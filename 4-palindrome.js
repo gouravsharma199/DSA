d485e6710c2893b01b0eed9fed14f963af4a6e57
gouravsharma199 <gouravsharma7227199@gmail.com>
2026-07-11 00:06:05 +0530

    palindrome for String

--- 4-palindrome.js
+++ 4-palindrome.js
@@ -24,3 +24,21 @@ function palindrome(num){
 
 // palindrome(-121);
 
+
+//palindrome for Strings
+function twoPointerPalindrome(str){
+    let left = 0;
+    let right = str.length -1;
+    
+    while(left<right){
+
+        if(str[left]!==str[right]) return false
+
+        left ++;
+        right --;
+    }
+    return true;
+
+}
+
+console.log(twoPointerPalindrome("madam"));
